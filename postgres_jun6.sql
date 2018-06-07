141e4cfde76c68eb5fd36c4387e4ee78206b9c0e
Chidam Raji <chidambaram.spv@gmail.com>
2018-06-06 23:42:24 -0700

    Multiple Common Table Expressions!
    This query entails the use of CTE and DATEDIFF function to set a conditional clause on temperatures on both dates.

---
+++ postgres_jun6.sql
@@ -0,0 +1,68 @@
+Write a SQL query to delete all duplicate email entries in a table named Person, keeping only unique emails based on its smallest Id.
+DELETE p1 FROM Person p1,
+    Person p2
+WHERE
+    p1.Email = p2.Email AND p1.Id > p2.Id
+
+
+
+The Trips table holds all taxi trips. Each trip has a unique Id, while Client_Id and Driver_Id are both foreign keys to the Users_Id at the Users table. Status is an ENUM type of (‘completed’, ‘cancelled_by_driver’, ‘cancelled_by_client’).
+
++----+-----------+-----------+---------+--------------------+----------+
+| Id | Client_Id | Driver_Id | City_Id |        Status      |Request_at|
++----+-----------+-----------+---------+--------------------+----------+
+| 1  |     1     |    10     |    1    |     completed      |2013-10-01|
+| 2  |     2     |    11     |    1    | cancelled_by_driver|2013-10-01|
+| 3  |     3     |    12     |    6    |     completed      |2013-10-01|
+| 4  |     4     |    13     |    6    | cancelled_by_client|2013-10-01|
+| 5  |     1     |    10     |    1    |     completed      |2013-10-02|
+| 6  |     2     |    11     |    6    |     completed      |2013-10-02|
+| 7  |     3     |    12     |    6    |     completed      |2013-10-02|
+| 8  |     2     |    12     |    12   |     completed      |2013-10-03|
+| 9  |     3     |    10     |    12   |     completed      |2013-10-03|
+| 10 |     4     |    13     |    12   | cancelled_by_driver|2013-10-03|
++----+-----------+-----------+---------+--------------------+----------+
+The Users table holds all users. Each user has an unique Users_Id, and Role is an ENUM type of (‘client’, ‘driver’, ‘partner’).
+
++----------+--------+--------+
+| Users_Id | Banned |  Role  |
++----------+--------+--------+
+|    1     |   No   | client |
+|    2     |   Yes  | client |
+|    3     |   No   | client |
+|    4     |   No   | client |
+|    10    |   No   | driver |
+|    11    |   No   | driver |
+|    12    |   No   | driver |
+|    13    |   No   | driver |
++----------+--------+--------+
+Write a SQL query to find the cancellation rate of requests made by unbanned users between Oct 1, 2013 and Oct 3, 2013. For the above tables, your SQL query should return the following rows with the cancellation rate being rounded to two decimal places.
+
++------------+-------------------+
+|     Day    | Cancellation Rate |
++------------+-------------------+
+| 2013-10-01 |       0.33        |
+| 2013-10-02 |       0.00        |
+| 2013-10-03 |       0.50        |
++------------+-------------------+
+
+WITH presel_tab AS
+(
+SELECT COUNT(Users.Users_Id) AS final_id, Users.Banned, Trips.Request_at AS date_details
+FROM Users
+JOIN Trips
+ON Users.Users_Id = Trips.Client_Id or Users.Users_Id = Trips.Driver_Id
+WHERE Trips.Status != 'completed' and Trips.Request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY date_details
+ORDER BY date_details
+)
+
+SELECT (CAST(s.ban_id) AS REAL/p.final_id) OVER(PARTITION BY p.date_details)
+FROM presel_tab p
+JOIN
+(SELECT COUNT(Users_Id) AS ban_id
+      FROM Users
+      WHERE Banned='No'
+)s
+ON p.final_id=s.ban_id
+GROUP BY p.date_details;
